1fa992a067d1cf1294e24ea15fec6985a6c9cb29
GeorgeAmany <106698653+GeorgeAmany@users.noreply.github.com>
2023-01-11 20:41:12 +0200

    update

--- lib/screens/login/view.dart
+++ lib/screens/login/view.dart
@@ -16,105 +16,92 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Padding(
-          padding:
-              const EdgeInsetsDirectional.only(start: 20, top: 20, bottom: 20),
-          child: IconButton(
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute (
-                builder: (BuildContext context) => const RegisterScreen(),
+
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 25 ,vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 51,
               ),
-              );
-            },
-            icon:
-                const Icon(Icons.arrow_back_ios_outlined, color: Colors.black),
-          ),
-        ),
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(horizontal: 25),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 51,
-            ),
-            const Text(
-              'LOGIN TO\nMY ACCOUNT',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
-            ),
-            const SizedBox(height: 38),
-            MyInput(
-              title: 'Email address ',
-            ),
-            MyInput(
-              title: 'password ',
-              isPassword: true,
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute (
-                  builder: (BuildContext context) => const ForgetPasswordScreen(),
-                ), );
-              },
-              child: const Text(
-                'Forgot Password',
-                style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black),
+              const Text(
+                'LOGIN TO\nMY ACCOUNT',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
               ),
-            ),
-            const SizedBox(
-              height: 100,
-            ),
-            Padding(
-              padding: const EdgeInsetsDirectional.only(start: 63, end: 63),
-              child: ElevatedButton(
+              const SizedBox(height: 38),
+              MyInput(
+                title: 'Email address ',
+              ),
+              MyInput(
+                title: 'password ',
+                isPassword: true,
+              ),
+              TextButton(
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (BuildContext context) =>
-                      const MyBottomNavigationBar(),
-                    ),
-                  );
+                  Navigator.push(context, MaterialPageRoute (
+                    builder: (BuildContext context) => const ForgetPasswordScreen(),
+                  ), );
                 },
-                style: ElevatedButton.styleFrom(
-                  fixedSize: const Size(294, 54),
-                ),
-                child: Text(
-                  'next'.toUpperCase(),
-                  style: const TextStyle(fontSize: 18),
+                child: const Text(
+                  'Forgot Password',
+                  style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsetsDirectional.only(start: 73),
-              child: Row(
-                children: [
-                  const Text(
-                    'New to app?',
-                    style: TextStyle(fontSize: 17, color: Color(0xff090808)),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute (
-                        builder: (BuildContext context) => const RegisterScreen(),
+              const SizedBox(
+                height: 100,
+              ),
+              Padding(
+                padding: const EdgeInsetsDirectional.only(start: 63, end: 63),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) =>
+                        const MyBottomNavigationBar(),
                       ),
-                      );
-                    },
-                    child: const Text(
-                      'Sign up',
-                      style: TextStyle(fontSize: 17, color: Color(0xff034C65)),
-                    ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(294, 54),
                   ),
-                ],
+                  child: Text(
+                    'next'.toUpperCase(),
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                ),
               ),
-            ),
-            const SizedBox(height: 120,),
-          ],
+              Padding(
+                padding: const EdgeInsetsDirectional.only(start: 73),
+                child: Row(
+                  children: [
+                    const Text(
+                      'New to app?',
+                      style: TextStyle(fontSize: 17, color: Color(0xff090808)),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute (
+                          builder: (BuildContext context) => const RegisterScreen(),
+                        ),
+                        );
+                      },
+                      child: const Text(
+                        'Sign up',
+                        style: TextStyle(fontSize: 17, color: Color(0xff034C65)),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 120,),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/register/view.dart
+++ lib/screens/register/view.dart
@@ -10,85 +10,72 @@ class RegisterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Padding(
-          padding:
-              const EdgeInsetsDirectional.only(start: 20, top: 20, bottom: 20),
-          child: IconButton(
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute (
-                builder: (BuildContext context) => const MyBottomNavigationBar(),
+
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 25 , vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 51,
               ),
-              );
-            },
-            icon:
-                const Icon(Icons.arrow_back_ios_outlined, color: Colors.black),
-          ),
-        ),
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(horizontal: 25),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 51,
-            ),
-            const Text(
-              'CREATE \nA NEW ACCOUNT',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
-            ),
-            const SizedBox(height: 38),
-            MyInput(
-              title: 'Full name ',
-            ),
-            MyInput(
-              title: 'Email address ',
-            ),
-            MyInput(
-              title: 'password ',
-              isPassword: true,
-            ),
-            const SizedBox(
-              height: 100,
-            ),
-            Padding(
-              padding: const EdgeInsetsDirectional.only(start: 63, end: 63),
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  fixedSize: const Size(294, 54),
-                ),
-                child: const Text(
-                  'CREATE ACCOUNT',
-                  style: TextStyle(fontSize: 18),
-                ),
+              const Text(
+                'CREATE \nA NEW ACCOUNT',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsetsDirectional.only(start: 60),
-              child: Row(
-                children: [
-                  const Text(
-                    'Already have an account?',
-                    style: TextStyle(fontSize: 17, color: Color(0xff090808)),
+              const SizedBox(height: 38),
+              MyInput(
+                title: 'Full name ',
+              ),
+              MyInput(
+                title: 'Email address ',
+              ),
+              MyInput(
+                title: 'password ',
+                isPassword: true,
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              Padding(
+                padding: const EdgeInsetsDirectional.only(start: 63, end: 63),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(294, 54),
                   ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute (
-                        builder: (BuildContext context) => const LoginScreen(),
-                      ),);
-                    },
-                    child: const Text(
-                      'Sign in',
-                      style: TextStyle(fontSize: 17, color: Color(0xff034C65)),
-                    ),
-                  )
-                ],
+                  child: const Text(
+                    'CREATE ACCOUNT',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
               ),
-            ),
-            const SizedBox(height: 120,),
-          ],
+              Padding(
+                padding: const EdgeInsetsDirectional.only(start: 60),
+                child: Row(
+                  children: [
+                    const Text(
+                      'Already have an account?',
+                      style: TextStyle(fontSize: 17, color: Color(0xff090808)),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute (
+                          builder: (BuildContext context) => const LoginScreen(),
+                        ),);
+                      },
+                      child: const Text(
+                        'Sign in',
+                        style: TextStyle(fontSize: 17, color: Color(0xff034C65)),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 120,),
+            ],
+          ),
         ),
       ),
     );
